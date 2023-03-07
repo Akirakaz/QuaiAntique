71df1b85abb321eb8ab435019c7c1b5a7ac433bb
Akirakaz <98087420+Akirakaz@users.noreply.github.com>
2023-03-07 04:38:34 +0100

    Add admin gallery controller

---
+++ src/Controller/admin/GalleryController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Controller\admin;
+
+use App\Entity\Image;
+use App\Form\ImageType;
+use App\Repository\ImageRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/admin/gallery')]
+class GalleryController extends AbstractController
+{
+    #[Route('/', name: 'app_admin_gallery_index', methods: ['GET'])]
+    public function index(ImageRepository $imageRepository): Response
+    {
+        return $this->render('admin/gallery/index.html.twig', [
+            'images' => $imageRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_admin_gallery_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, ImageRepository $imageRepository): Response
+    {
+        $image = new Image();
+        $form = $this->createForm(ImageType::class, $image);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $imageRepository->save($image, true);
+
+            return $this->redirectToRoute('app_admin_gallery_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('admin/gallery/new.html.twig', [
+            'image' => $image,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_admin_gallery_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Image $image, ImageRepository $imageRepository): Response
+    {
+        $form = $this->createForm(ImageType::class, $image);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $imageRepository->save($image, true);
+
+            return $this->redirectToRoute('app_admin_gallery_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('admin/gallery/edit.html.twig', [
+            'image' => $image,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_admin_gallery_delete', methods: ['POST'])]
+    public function delete(Request $request, Image $image, ImageRepository $imageRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$image->getId(), $request->request->get('_token'))) {
+            $imageRepository->remove($image, true);
+        }
+
+        return $this->redirectToRoute('app_admin_gallery_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
