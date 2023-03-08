f294e04113352d0daeea66cfab258cd75610fcef
Akirakaz <98087420+Akirakaz@users.noreply.github.com>
2023-03-08 18:55:46 +0100

    Move and rename controller

--- src/Controller/admin/MealsCrudController.php
+++ src/Controller/admin/MealsCrudController.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\admin;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class MealsController extends AbstractController
+class MealsCrudController extends AbstractController
 {
     #[Route('/meals', name: 'app_meals')]
     public function index(): Response
