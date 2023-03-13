fb5eab82a318cfe55fe4d956942f786f51360d0a
Akirakaz <98087420+Akirakaz@users.noreply.github.com>
2023-03-13 20:23:44 +0100

    Add menu CRUD & Templates

---
+++ src/Form/MenuType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Menu;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class MenuType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'label' => 'Titre',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'menu.title.not_blank'
+                    ])
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Menu::class,
+        ]);
+    }
+}
