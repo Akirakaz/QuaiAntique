4f2f55ee4122fe2d63c155815b38210f2afb9d14
Guillaume SIONNEAU <guillaume.sionneau@gmail.com>
2023-03-18 15:36:13 +0100

    Update path controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,18 @@
 
 namespace App\Controller;
 
+use App\Repository\ImageRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function index(): Response
+    #[Route('/', name: 'app_home', methods: ['GET'])]
+    public function index(ImageRepository $imageRepository): Response
     {
         return $this->render('public/home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'images' => $imageRepository->findAll(),
         ]);
     }
 }
